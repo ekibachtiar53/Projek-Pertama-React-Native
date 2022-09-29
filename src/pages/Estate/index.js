269d5f6c7b3727791eff2f5efcdb9ffab2c5cef6
ekibachtiar <ekibachtiar53@gmail.com>
2022-09-29 13:53:14 +0700

    pembaruan

--- src/pages/Estate/index.js
+++ src/pages/Estate/index.js
@@ -4,13 +4,28 @@ import {StyleSheet, Text, View} from "react-native";
 const Estate = () => {
   return (
     <View>
-      <Text>Estate</Text>
+      <Text style={styles.text1}>Estate</Text>
+      <Text style={styles.text2}>Best discovery ever</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  text1: {
+    fontFamily: 'poppins',
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#151619',
+    marginLeft: 24,
+    marginTop: 30
+  },
+  text2: {
+    fontFamily: 'poppins',
+    fontSize: 14,
+    color: '#6B6978',
+    marginLeft: 24,
+    marginTop: 10
+  }
 })
 
 
